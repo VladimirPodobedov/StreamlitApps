5282f5a4b185e3ac29f09b9747e28105381634f7
VladimirPodobedov <podobedov@tut.by>
2025-02-11 11:39:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,81 @@
-# программа загружает выбранный excel файл в pandas, читает имена столбцов и делает sql запрос и выводит результат
-
 # программа делает следующее:
 # Загружает выбранный Excel-файл в pandas DataFrame.
-# Выводит имена столбцов.
-# Создает временную базу данных SQLite.
-# Записывает данные из DataFrame в эту базу.
-# Выполняет простой SQL-запрос (SELECT *) на этих данных.
-# Чтобы использовать более сложные SQL-запросы или конкретные условия фильтрации, просто измените строку query внутри функции execute_sql_query.
+# Посылает в чат gpt пользовательский запрос и получает SQL.
+# Выполняет SQL-запрос (SELECT *) на этих данных.
 
+import os
+from groq import Groq
+#from openai import OpenAI
 import streamlit as st
-from openai import OpenAI
 import pandas as pd
 import pandasql as ps
 import sqlite3
 import re
 
 
-OPEN_KEY = "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"
-
 fields = "name, price"
-conditions = "Если говорю моя машина, то имей в виду Porsсhe."
-question = "сколько стоит моя машина"
+conditions = "" #My car is Porsche
+question = "" #What is my car?
 
+#------------------------------------------------------------------------------
 def extract_substring_regex(text):
     pattern = r'(?<=\`\`\`sql).*?(?=\`\`\`)'
     match = re.search(pattern, text, flags=re.DOTALL)
     
     if match:
         return match.group()
-        
-        
-def output_title(str):
+       
+def add_last_char_if_needed(str,ch):
+    if str and (str[-1:] != ch):
+        str = str + ch
+    return str
+    
+#------------------------------------------------------------------------------
+def UI_create_title(str):
     #print(str)
     st.title(str)
 
-def output_str(str):
+#------------------------------------------------------------------------------
+def UI_output_str(str):
     #print(str)
     st.write(str)
 
-def get_excel_file(prompt, file_type):
+#------------------------------------------------------------------------------
+def UI_get_string(prompt_string):
+    return st.text_input(prompt_string)
+    
+#------------------------------------------------------------------------------
+def UI_get_excel_file(prompt, file_type):
     return st.file_uploader(prompt+" ("+file_type+")", type=[file_type])
 
+
+#------------------------------------------------------------------------------
+# Groq - Chat GPT
+# https://console.groq.com/playground
+# org-id: org_01jkstbcz9e2ss1s5cdtvfaawd
+def ai_groq_get_query(user_query_string):
+    client = Groq( 
+        api_key="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" 
+    )
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_query_string,
+                }
+            ],
+            #model="llama-3.3-70b-versatile",
+            model="gemma2-9b-it",
+        )
+        return chat_completion.choices[0].message.content
+
+    except Exception as e:
+
+        UI_output_str(f"Ошибка обращения к GPT: {e}")
+        return None
+
+#------------------------------------------------------------------------------
 # Функция для загрузки данных из Excel-файла в DataFrame
 def load_excel_to_df(file_path):
     try:
@@ -48,96 +83,39 @@ def load_excel_to_df(file_path):
         df = pd.read_excel(file_path)
         return df
     except Exception as e:
-        output_str(f"Ошибка при загрузке файла: {e}")
+        UI_output_str(f"Ошибка при загрузке файла: {e}")
         return None
 
-# Функция для создания базы данных SQLite и выполнения запроса
-def execute_sql_query(df):
-    try:
-        # Создание базы данных SQLite в памяти (для простоты)
-        conn = sqlite3.connect(':memory:')
-        
-        # Запись DataFrame в таблицу базы данных SQLite
-        df.to_sql('data', conn, if_exists='replace', index=False)
-
-        # Выполнение SQL-запроса (например, выборка всех столбцов)
-        cursor = conn.cursor()
-        
-        # Прочитаем все столбцы из DataFrame (теперь таблицы 'data')
-        columns = list(df.columns)
-        
-        query = f"SELECT * FROM data LIMIT 2"
-        cursor.execute(query)
-        
-        rows = cursor.fetchall()
-        
-        for row in rows:
-            output_str(row)  # Выводим результат
-        
-    except Exception as e:
-        output_str(f"Ошибка при выполнении запроса: {e}")
 
 # Основная функция программы
 def main():
 
     # Создание интерфейса для выбора файла
-    output_title("Natural language SQL")
-    
-#    file_rules = get_excel_file("Select the rule file", 'xls')
-    file_data = get_excel_file("Select the data file", 'xls')    
-    
-    conditions = st.text_input("Write conditions here")
-    if conditions[-1:] != '.':
-        conditions = conditions + '.'
-        
-    question = st.text_input("Write the query string here")
-    if question[-1:] != '?':
-        question = question + '?'
+    UI_create_title("Natural language SQL")
+    #file_rules = UI_get_excel_file("Select the rule file", 'xls')
+    file_data = UI_get_excel_file("Select the data file", 'xls')    
+    conditions = add_last_char_if_needed(st.text_input("Write conditions here"),'.')
+    question = add_last_char_if_needed(st.text_input("Write the query string here"),'?')
     
     if st.button(" RUN "):
     
-#        df1 = load_excel_to_df(file_rules)
+        #df1 = load_excel_to_df(file_rules)
         df = load_excel_to_df(file_data)
         if df is not None:
-        
-        
+      
             fields = ', '.join(list(df.columns))
-            #output_str(result_string)
         
-            # Выполняем запрос и выводим результат
-            #execute_sql_query(df)
-            
             if not question:
                 sql_query = """SELECT * FROM df """
             else:
                 user_query = "Show only SQL. The table df has columns: "+fields+". "+conditions +" "+ question
-                output_str(user_query)
-                
-                client = OpenAI(
-                    base_url="https://openrouter.ai/api/v1",
-                    api_key=OPEN_KEY,
-                )
-                try:
-                    completion = client.chat.completions.create(
-                        extra_headers={
-                            "HTTP-Referer": "https://gsktb.com", # Optional. Site URL for rankings on openrouter.ai.
-                            "X-Title": "https://gsktb.com", # Optional. Site title for rankings on openrouter.ai.
-                        },
-                        extra_body={},
-                        model="deepseek/deepseek-r1:free",
-                        messages=[{
-                            "role": "user",
-                            "content": user_query
-                        }]
-                    )
-                except Exception as e:
-                    output_str(f"Ошибка при выполнении запроса: {e}")
-                    
-                sql_query = extract_substring_regex(completion.choices[0].message.content)
+                UI_output_str(user_query)
+                sql_query = extract_substring_regex(ai_groq_get_query(user_query))
             
-            output_str(f"SQL: {sql_query}")
-            output_str(ps.sqldf(sql_query, locals()))
+            if sql_query is not None:
 
+                UI_output_str(f"SQL: {sql_query}")
+                UI_output_str(ps.sqldf(sql_query, locals()))
 
             
 if __name__ == "__main__":
